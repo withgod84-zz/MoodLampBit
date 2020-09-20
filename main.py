0425c168b2154792a118bb6602137cb39be178e1
withgod84 <52286388+withgod84@users.noreply.github.com>
2020-09-21 05:18:35 +0900

    A: LED 숫자 변경, B: 램프 ON/OFF, 흔들기: 램프 ON/OFF

--- main.py
+++ main.py
@@ -1,35 +1,84 @@
 def on_button_pressed_a():
-    global counter
-    counter += 1
-    if counter > 3:
-        counter = 0
-    basic.show_number(counter)
+    global LED숫자, LED타이머
+    LED숫자 += 1
+    LED숫자 = LED숫자 % 4
+    basic.show_number(LED숫자)
+    LED타이머 = 6
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_gesture_shake():
+    global 램프상태, LED타이머
+    if 램프모드 == 1:
+        if 램프상태 == 0:
+            램프상태 = 1
+        else:
+            램프상태 = 0
+    basic.show_number(LED숫자)
+    LED타이머 = 6
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 def on_button_pressed_ab():
-    if counter == 0:
-        strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
-    elif counter == 1:
-        strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
-    elif counter == 2:
-        strip.show_color(neopixel.colors(NeoPixelColors.BLUE))
+    global 램프모드, 램프상태, LED타이머
+    램프모드 = 0
+    if 램프상태 == 1:
+        램프상태 = 0
     else:
-        strip.show_rainbow(1, 360)
-        while True:
-            strip.rotate(1)
-            strip.show()
-            basic.pause(500)
+        램프상태 = 1
+    basic.show_number(LED숫자)
+    LED타이머 = 6
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    global counter
-    counter += -1
-    if counter < 0:
-        counter = 0
-    basic.show_number(counter)
+    global 램프모드, LED타이머
+    램프모드 += 1
+    램프모드 = 램프모드 % 3
+    basic.show_number(램프모드)
+    LED타이머 = 6
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-strip: neopixel.Strip = None
-counter = 0
-basic.show_number(counter)
+램프상태 = 0
+램프모드 = 0
+LED타이머 = 0
+LED숫자 = 0
+basic.show_number(LED숫자)
 strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+strip.set_brightness(100)
+
+def on_forever():
+    global 램프상태
+    if 램프모드 == 2:
+        if input.light_level() < 60:
+            램프상태 = 1
+        else:
+            램프상태 = 0
+    basic.pause(500)
+basic.forever(on_forever)
+
+def on_forever2():
+    if 램프상태 == 1:
+        if LED숫자 == 0:
+            strip.show_color(neopixel.colors(NeoPixelColors.ORANGE))
+        elif LED숫자 == 1:
+            strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+        elif LED숫자 == 2:
+            strip.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+        else:
+            strip.show_rainbow(1, 360)
+            while LED숫자 == 3 and 램프상태 == 1:
+                strip.rotate(1)
+                strip.show()
+                basic.pause(500)
+    else:
+        strip.clear()
+        strip.show()
+    basic.pause(500)
+basic.forever(on_forever2)
+
+def on_forever3():
+    global LED타이머
+    if LED타이머 > 0:
+        LED타이머 += -1
+        if LED타이머 == 0:
+            basic.clear_screen()
+    basic.pause(500)
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,53 +1,29 @@
 input.onButtonPressed(Button.A, function () {
     LED숫자 += 1
-    LED숫자 = LED숫자 % 4
+    if (LED숫자 == 4) {
+        LED숫자 = 0
+    }
     basic.showNumber(LED숫자)
-    LED타이머 = 6
 })
 input.onGesture(Gesture.Shake, function () {
-    if (램프모드 == 1) {
-        if (램프상태 == 0) {
-            램프상태 = 1
-        } else {
-            램프상태 = 0
-        }
-    }
-    basic.showNumber(LED숫자)
-    LED타이머 = 6
-})
-input.onButtonPressed(Button.AB, function () {
-    램프모드 = 0
-    if (램프상태 == 1) {
-        램프상태 = 0
-    } else {
+    if (램프상태 == 0) {
         램프상태 = 1
+    } else {
+        램프상태 = 0
     }
-    basic.showNumber(LED숫자)
-    LED타이머 = 6
 })
 input.onButtonPressed(Button.B, function () {
-    램프모드 += 1
-    램프모드 = 램프모드 % 3
-    basic.showNumber(램프모드)
-    LED타이머 = 6
+    if (램프상태 == 0) {
+        램프상태 = 1
+    } else {
+        램프상태 = 0
+    }
 })
 let 램프상태 = 0
-let 램프모드 = 0
-let LED타이머 = 0
 let LED숫자 = 0
 basic.showNumber(LED숫자)
 let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 strip.setBrightness(100)
-basic.forever(function () {
-    if (램프모드 == 2) {
-        if (input.lightLevel() < 60) {
-            램프상태 = 1
-        } else {
-            램프상태 = 0
-        }
-    }
-    basic.pause(500)
-})
 basic.forever(function () {
     if (램프상태 == 1) {
         if (LED숫자 == 0) {
@@ -58,24 +34,15 @@ basic.forever(function () {
             strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
         } else {
             strip.showRainbow(1, 360)
-            while (LED숫자 == 3 && 램프상태 == 1) {
+            while (LED숫자 == 3 && true) {
                 strip.rotate(1)
                 strip.show()
-                basic.pause(500)
+                basic.pause(1000)
             }
         }
     } else {
         strip.clear()
         strip.show()
     }
-    basic.pause(500)
-})
-basic.forever(function () {
-    if (LED타이머 > 0) {
-        LED타이머 += -1
-        if (LED타이머 == 0) {
-            basic.clearScreen()
-        }
-    }
-    basic.pause(500)
+    basic.pause(1000)
 })
