bc5b5a7fc04b1bcd88601465787c5e74de3a7ac1
withgod84 <52286388+withgod84@users.noreply.github.com>
2020-09-20 11:55:06 +0900

    minor

--- main.ts
+++ main.ts
@@ -2,14 +2,15 @@ input.onButtonPressed(Button.A, function () {
     LED숫자 += 1
     LED숫자 = LED숫자 % 4
     basic.showNumber(LED숫자)
-    LED타이머 += 5
+    LED타이머 = 6
 })
 input.onGesture(Gesture.Shake, function () {
     if (램프모드 == 1) {
+        램프상태 = 1
         LED숫자 += 1
         LED숫자 = LED숫자 % 4
         basic.showNumber(LED숫자)
-        LED타이머 += 5
+        LED타이머 = 6
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -20,13 +21,13 @@ input.onButtonPressed(Button.AB, function () {
         램프상태 = 1
     }
     basic.showNumber(LED숫자)
-    LED타이머 += 5
+    LED타이머 = 6
 })
 input.onButtonPressed(Button.B, function () {
     램프모드 += 1
     램프모드 = 램프모드 % 3
     basic.showNumber(램프모드)
-    LED타이머 += 5
+    LED타이머 = 6
 })
 let 램프상태 = 0
 let 램프모드 = 0
@@ -42,15 +43,6 @@ basic.forever(function () {
         } else {
             램프상태 = 0
         }
-    }
-    basic.pause(500)
-})
-basic.forever(function () {
-    if (LED타이머 > 0) {
-        LED타이머 += -1
-    }
-    if (LED타이머 == 0) {
-        basic.clearScreen()
     }
     basic.pause(500)
 })
@@ -73,6 +65,15 @@ basic.forever(function () {
     } else {
         strip.clear()
         strip.show()
+    }
+    basic.pause(500)
+})
+basic.forever(function () {
+    if (LED타이머 > 0) {
+        LED타이머 += -1
+        if (LED타이머 == 0) {
+            basic.clearScreen()
+        }
     }
     basic.pause(500)
 })
